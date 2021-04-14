1d60d55d3ea9a44eb63724f70b84759319f89ca9
Elena Andrews <elenaandrews2022@u.northwestern.edu>
2021-04-14 13:57:36 -0500

    Loaded data into data cleaning r script and made it a tibble!

---
+++ output/data processing/data_cleaning.R
@@ -0,0 +1,10 @@
+# Load Necessary Packages ------------------------------------------------
+library(tidyverse)
+
+
+# Load Data ---------------------------------------------------------------
+load(file = "data/unprocessed/local_arts_data.rda")
+
+
+# Make Data a Tibble ------------------------------------------------------
+as_tibble(da35586.0001)
