8acf5be1b9e5b8fd3038a715500abfbf8b676c62
Elena Andrews <elenaandrews2022@u.northwestern.edu>
2021-04-15 18:54:17 -0500

    Split data into training and testing sets.

--- output/eda/eda_scratch_work.R
+++ output/eda/eda_scratch_work.R
@@ -1,14 +1,32 @@
 # Load Necessary Packages -------------------------------------------------
 library(tidyverse)
 library(naniar)
+library(tidymodels)
 
 # Load in Data ------------------------------------------------------------
 load(file = "data/processed/local_arts_data.Rda")
 
 # Create a Graph of the Outcome Variable ----------------------------------
 local_arts_data %>% 
-  filter(!is.na(income) & income != "(11) Refused" & income != "(10) Don't know") %>% 
+  filter(!is.na(income) & income != "Refused" & income != "Don't know") %>% 
   ggplot(aes(x = income)) +
-  geom_bar() +
-  coord_flip()
+  geom_bar(fill = "#3b4b21") +
+  coord_flip() +
+  labs(
+    title = "Distribution of Respondent's Household Income"
+  ) +
+  theme_minimal()
+
+# Split Data --------------------------------------------------------------
+# conduct initial split
+local_arts_split <- initial_split(data = local_arts_data, prop = 0.7, strata = income)
+
+# collect training data
+local_arts_train <- training(local_arts_split)
+
+# collect testing data
+local_arts_test <- testing(local_arts_split)
+
+# More EDA ---------------------------------------------------------------
+
 
