e0fbfeda2a5c06b60de16e586c0afefe5788b667
Derex Wangmang <dwangmang@gmail.com>
2021-04-14 21:07:16 -0500

    Cleaned datset by replacing some NA's with 0's

--- output/data processing/data_cleaning.R
+++ output/data processing/data_cleaning.R
@@ -7,4 +7,17 @@ load(file = "data/unprocessed/local_arts_data.rda")
 
 
 # Make Data a Tibble ------------------------------------------------------
-as_tibble(da35586.0001)
+local_arts_data <- as_tibble(da35586.0001)
+all_col_names <- colnames(local_arts_data)
+
+for (i in 1:(length(all_col_names) - 1)) {
+  if (all_col_names[i] == substring(all_col_names[i+1], 2)) {
+    for (j in 1:(nrow(local_arts_data[i]))) {
+      if (local_arts_data[j,i] == "(2) No") {
+        local_arts_data[j, i+1] = 0
+      }
+    }
+  }
+}
+
+save(local_arts_data, file = "data/processed/local_arts_data.Rda")
