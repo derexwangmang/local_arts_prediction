b33f65ed20dec8b0a19767b67a274e66bda4731e
Elena Andrews <elenaandrews2022@u.northwestern.edu>
2021-04-14 22:17:05 -0500

    Added labels to data cleaning, and added step to clean variable names.

--- output/data processing/data_cleaning.R
+++ output/data processing/data_cleaning.R
@@ -1,5 +1,6 @@
 # Load Necessary Packages ------------------------------------------------
 library(tidyverse)
+library(janitor)
 
 
 # Load Data ---------------------------------------------------------------
@@ -8,6 +9,9 @@ load(file = "data/unprocessed/local_arts_data.rda")
 
 # Make Data a Tibble ------------------------------------------------------
 local_arts_data <- as_tibble(da35586.0001)
+
+
+# Use a Loop To Make Appropriate Missing Values Zero ----------------------
 all_col_names <- colnames(local_arts_data)
 
 for (i in 1:(length(all_col_names) - 1)) {
@@ -20,4 +24,9 @@ for (i in 1:(length(all_col_names) - 1)) {
   }
 }
 
+# Clean Variable Names ----------------------------------------------------
+local_arts_data <- local_arts_data %>% 
+  clean_names()
+
+# Write Out Processed Data Set --------------------------------------------
 save(local_arts_data, file = "data/processed/local_arts_data.Rda")
