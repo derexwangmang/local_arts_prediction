c37dd0ddecaa77b8c86efe00c2f0eb68589c869e
Elena Andrews <elenaandrews2022@u.northwestern.edu>
2021-05-24 20:52:27 -0500

    Added to final report, evaluated more models.

--- output/models/evaluation.R
+++ output/models/evaluation.R
@@ -16,3 +16,33 @@ load(file = "output/models/rf_tuned.rda")
 rf_tuned %>% 
   show_best(metric = "roc_auc")
 
+# Load Boosted Tree Tuned Object ------------------------------------------
+load(file = "output/models/bt_tuned.rda")
+
+# Evaluated Boosted Tree Model --------------------------------------------
+bt_tuned %>% 
+  show_best(metric = "roc_auc")
+
+# Load Support Vector Machine (Radial Basis Function Kernel) Tuned Object --------------------------------------
+load(file = "output/models/svm_tuned.rda")
+
+# Evaluate Support Vector Machine (Radial Basis Function Kernel) ----------
+svm_tuned %>% 
+  show_best(metric = "roc_auc")
+
+# Load Neural Network Tuned Object -----------------------------------------------------
+load(file = "output/models/neural_tuned.rda")
+
+# Evaluate Neural Network -------------------------------------------------
+nn_tuned %>% 
+  show_best(metric = "roc_auc")
+
+# Load K-Nearest Neighbors Model --------------------------------------
+load(file = "output/models/knn_tuned.rda")
+
+
+# Evaluate K-Nearest Neighbors Model --------------------------------------
+knn_tuned %>% 
+  show_best(metric = "roc_auc")
+
+
