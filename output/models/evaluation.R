d0e11aa8a13cbdb82ddbca30945475d3858f127e
Elena Andrews <elenaandrews2022@u.northwestern.edu>
2021-05-31 20:22:14 -0500

    Added to final report.

--- output/models/evaluation.R
+++ output/models/evaluation.R
@@ -1,6 +1,8 @@
 # Load Necessary Packages -------------------------------------------------
 library(tidyverse)
 library(tidymodels)
+# install.packages("kableExtra")
+library(kableExtra)
 
 # Load Elastic Net Tuned Object -------------------------------------------
 load(file = "output/models/en_tuned.rda")
@@ -13,8 +15,13 @@ en_tuned %>%
 load(file = "output/models/rf_tuned.rda")
 
 # Evaluated Random Forest Model -------------------------------------------
+# show best
 rf_tuned %>% 
   show_best(metric = "roc_auc")
+
+# autoplot
+# specify roc_auc as my performance measure
+autoplot(rf_tuned, metric = "roc_auc")
 
 # Load Boosted Tree Tuned Object ------------------------------------------
 load(file = "output/models/bt_tuned.rda")
@@ -46,6 +53,24 @@ knn_tuned %>%
   show_best(metric = "roc_auc")
 
 
+# Creating table of roc-auc values and standard error of best model --------
+# create tibble
+best_models <- tribble(
+  ~`Model Type`, ~Roc-auc, ~`Standard error`,
+  #----------|---------|----------------|
+  "Random Forest", 0.712, 0.00441,
+  "Elastic Net", 0.684, 0.00264,
+  "Boosted Tree", 0.688, 0.00265,
+  "Support Vector Machine", 0.687, 0.00306,
+  "Neural Network", 0.591, 0.0128,
+  "K-Nearest Neighbors", 0.596, 0.00385
+)
+
+# arrange in descending order of roc-auc
+best_models %>% 
+  arrange(desc(roc_auc))
+
+
 # Finalizing Best Model (Random Forest) -----------------------------------
 rf_final_workflow <- rf_workflow %>% 
   finalize_workflow(select_best(rf_tuned, metric = "roc_auc"))
@@ -72,12 +97,14 @@ rf_final_pred <- predict(rf_final_results, new_data = local_arts_test %>% select
 
 
 # Evaluate Predicted Values -----------------------------------------------
-local_arts_metrics <- metric_set(roc_auc, accuracy)
-
 # apply predicted values to roc_auc to see how our model performed
 rf_final_pred %>%
-  local_arts_metrics(truth = income, estimate = .pred_class, `.pred_Under $10,000`, `.pred_$10,000 to $14,999`, `.pred_$15,000 to $19,999`,
+  roc_auc(truth = income, `.pred_Under $10,000`, `.pred_$10,000 to $14,999`, `.pred_$15,000 to $19,999`,
                      `.pred_$20,000 to $29,999`, `.pred_$30,000 to $39,999`, `.pred_$40,000 to $49,999`,
                      `.pred_$50,000 to $74,999`, `.pred_$75,000 to $99,999`, `.pred_$100,000 or more`)
 
+# save out predicted values of random forest model on test data
 save(rf_final_pred, "output/models/random_forest_predictions.rda")
+
+# read in predicted values
+load(file = "output/models/random_forest_predictions.rda")
